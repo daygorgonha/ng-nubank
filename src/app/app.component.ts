46fc972fadc3cc6a5fa239d1712f170b1af9f2ff
Dayane Gorgonha <dayanebismark18@gmail.com>
2022-10-22 10:47:54 -0300

    feat: account

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import  KeenSlider, { KeenSliderInstance } from 'keen-slider';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [
+    './app.component.scss',
+    '../../node_modules/keen-slider/keen-slider.min.css'
+  ]
 })
 export class AppComponent {
   title = 'ng-nubank';
+
+  @ViewChild('sliderRef')
+  sliderRef!: ElementRef<HTMLElement>
+
+  slider?: KeenSliderInstance
+
+  ngAfterViewInit() {
+    this.slider = new KeenSlider(this.sliderRef.nativeElement, {
+      loop: false,
+      mode: 'free',
+      slides: {
+        perView: 4.5,
+        spacing: 14
+      }
+    })
+  }
+
+
 }
